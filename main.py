2a94d7634663a1ecc856f21fc15c39977ce9cfa6
Vivek Dhir <vivekdhir@Viveks-MacBook-Pro.local>
2023-08-29 01:35:04 +0530

    adding datasets

--- main.py
+++ main.py
@@ -2,6 +2,35 @@ from c_means import *
 from Quantum_solver import *
 import pandas as pd
 
+def main(df, labels, generations, distType=0):
+    f = [0.707 for i in range(2)]
+    quant = Quantum_solver(extremum=1, features = f) # we are using this to maximize
+    m = search_area(quant.solver()+1) # search space: range of values between 1.5 to 2.5
+    global si
+    for gen in range(generations):
+        # print(m)
+        cluster = Cmeans(df, labels, epsilon=1, distType=distType, m=m)
+        cluster.c_means()
+        si = cluster.getSI()
+        m = search_area(quant.solver(si)+1) # we are trying to maximize SI closer to 1
+    return si 
+
+def norm(df):
+    df_new = []
+    for row in df: # row/sqrt(f1^2 + f2^2 + f3^2...)
+        denominator = 0
+        for feature in row:
+            denominator += (feature*feature)
+        denominator = math.sqrt(denominator)
+        record = []
+        for feature in row:
+            try:
+                record.append(feature/denominator)
+            except:
+                record.append(0)
+        df_new.append(record)
+    df = df_new
+    return df
 
 if __name__ == "__main__":
     filename = "./iris.csv"
@@ -11,22 +40,13 @@ if __name__ == "__main__":
     df = pd.read_csv(filename)
     labels = df[target_column] # actual labels
     df = df.drop([target_column], axis=1) # dropping labels column
-
-
     df = df.values.tolist()
+    df_normalized = norm(df)
     labels = labels.values.tolist()
-
-    f = [0.707 for i in range(2)]
-    quant = Quantum_solver(extremum=1, features = f) # we are using this to maximize
-    m = search_area(quant.solver()+1) # search space: range of values between 1.5 to 2.5
-    global si
-    for gen in range(generations):
-        # print(m)
-        cluster = Cmeans(df, labels, epsilon=1, distType=0, m=m)
-        cluster.c_means()
-        si = cluster.getSI()
-        m = search_area(quant.solver(si)+1) # we are trying to maximize SI closer to 1 
-    print(f"SI after {generations} generations: ",si)
+    si1 = main(df, labels, generations)
+    si2 = main(df_normalized, labels, generations, 1)
+    print(f"(non qunatum) SI after {generations} generations: ",si1)
+    print(f"(qunatum) SI after {generations} generations: ",si2)
 
 
 
